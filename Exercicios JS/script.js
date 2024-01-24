4b4b651abb384c4d23c10e7c186ea0e27196f4fc
guimarts1 <martins768ferreira@gmail.com>
2024-01-24 20:37:47 -0300

    att js

--- Exercicios JS/script.js
+++ Exercicios JS/script.js
@@ -1,1 +1,21 @@
-alert('ola')
+function carregar(){
+    var msg = document.querySelector('.msg')
+    var img = document.querySelector('.img')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML =`Agora são ${hora} horas.`
+    
+    if (hora >= 0 && hora < 12) {
+        // bom dia.
+        img.src = 'images/manha.png'
+        document.body.style.background = 'yellow'
+    } else if (hora >= 12 && hora < 18) {
+        // boa tarde.
+        img.src = 'images/tarde.png'
+        document.body.style.background = 'orange'
+    } else {
+        //boa noite.
+        img.src = 'images/noite.png'
+        document.body.style.background = 'grey'
+    }
+}
