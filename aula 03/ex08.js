f929579c410ada5319dd4c75b7d02e2f310d17bc
guimarts1 <martins768ferreira@gmail.com>
2024-01-25 19:35:51 -0300

    att js

---
+++ aula 03/ex08.js
@@ -0,0 +1,37 @@
+console.log('vai começar...')
+for(var c = 1;c <= 4;c++){
+    console.log(c)
+}
+console.log('fim')
+
+a
+
+
+
+/*var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+
+} while (c <= 6) */
+
+
+
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+*/ 
+
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?') */
+
+
+
